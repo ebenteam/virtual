c5593f773ee93a6722ef4e8b325effe11d8a2fdd
ebenteam <dcm1net@gmail.com>
2018-11-08 17:49:10 -0500

    proyecto base virtual configurando template

---
+++ README.md
@@ -0,0 +1,4 @@
+virtual
+=======
+
+A Symfony project created on November 8, 2018, 4:36 pm.
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('template/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/template", name="template")
+     */
+    public function templateAction(Request $request)
+    {
+        // ruta para ver el template
+        return $this->render('template/layout.adminlte.html.twig', 
+        array(
+        )
+        );
+    }
+}
