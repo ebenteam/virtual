9ccfb185ca0964d72d9f9fe87b8927db59d39b2a
ebenteam <dcm1net@gmail.com>
2018-11-09 00:10:29 -0500

    plantilla adminlte funcionando, usar extends y crear bloques

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -8,26 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('template/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
-    }
-
+    
     /**
      * @Route("/template", name="template")
      */
     public function templateAction(Request $request)
     {
         // ruta para ver el template
-        return $this->render('template/layout.adminlte.html.twig', 
-        array(
-        )
-        );
+        return $this->render('layout.adminlte.html.twig');
     }
 }
